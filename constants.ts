7aaaa89d9068e33759cee37856a3fa2771caf216
Sam Li <weisen.li@hotmail.com>
2026-06-25 15:15:07 +0000

    remove random movie api

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://samliweisen.onrender.com/api/';
+export const API_URL = "https://samliweisen.onrender.com/api/";
 
 export const LOGIN_API = `${API_URL}user/login`;
 
@@ -29,5 +29,3 @@ export const VIDEO_LIST_API = `${API_URL}videos/bilibili`;
 export const STATS_LIST_API = `${API_URL}stats`;
 
 export const USER_AUTH_API = `${API_URL}user/auth`;
-
-export const MOVIE_RANGDOM_API = `${API_URL}movie/random`;
