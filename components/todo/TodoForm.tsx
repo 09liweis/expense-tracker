fd7fda396519e2412775d77829f8b72cd574df6d
09liweis <weisen.li@hotmail.com>
2026-09-17 15:45:55 -0400

    add cursor to close todo form

--- components/todo/TodoForm.tsx
+++ components/todo/TodoForm.tsx
@@ -32,7 +32,7 @@ export default function TodoForm({ onSubmit, onClose }: TodoFormProps) {
           <button
             type="button"
             onClick={onClose}
-            className="text-gray-500 hover:text-gray-700 transition-colors"
+            className="text-gray-500 hover:text-gray-700 transition-colors cursor-pointer"
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
