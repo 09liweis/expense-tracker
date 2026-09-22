a971c5b75c8dfc9cfb73d87194b0d27c2673b309
09liweis <weisen.li@hotmail.com>
2026-09-22 11:38:55 -0400

    update todoInput type

--- components/todo/TodoInput.tsx
+++ components/todo/TodoInput.tsx
@@ -1,4 +1,13 @@
-export default function TodoInput({field, title, value, type="text", onChange, placeholder=""}) {
+interface InputProps {
+  field: string;
+  value: string;
+  title: string;
+  type: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
+export default function TodoInput({field, title, value, type="text", onChange, placeholder=""}:InputProps) {
   return (
     <div key={field}>
       <label htmlFor={field} className="block text-sm font-medium text-gray-700 mb-1">
@@ -8,7 +17,7 @@ export default function TodoInput({field, title, value, type="text", onChange, p
         id={field}
         type={type}
         value={value}
-        onChange={(e) => onChange(e)}
+        onChange={onChange}
         className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors"
         placeholder={placeholder}
         required
