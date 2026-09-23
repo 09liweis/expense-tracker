600cdd5814aa2a731a5e6e8c9a6ee276f8f1ceda
09liweis <weisen.li@hotmail.com>
2026-09-23 11:30:21 -0400

    update todo loc link

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -46,7 +46,7 @@ export default function TodoList({
                 {todo.name}
               </span>
               {todo.loc?.addr && 
-                <div className="text-sm text-gray-400">{todo.loc.addr}</div>
+                <a target="_blank" href={`https://www.google.com/maps/place/${todo.loc?.addr}`} className="text-sm text-gray-400">{todo.loc.addr}</a>
               }
             </div>
           </div>
